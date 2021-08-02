c2257bf5bd8810ecd590361742cd0db28cc913df
Jahanzaib Sheikh <53260475+jahanzaibsheikh830@users.noreply.github.com>
2021-08-02 11:39:42 +0500

    Add files via upload

--- components/listItems.tsx
+++ components/listItems.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Btn from "./button";
+import TextField from "./textfield";
 interface Todosprops {
   todos: string[];
   setTodosItem: (value: React.SetStateAction<string[]>) => void;
 }
 function ListItem({ todos, setTodosItem }: Todosprops) {
+  const [isTrue, setIsTrue] = useState(false);
+  const [updateTodoInput, setUpdateTodoInput] = useState("");
+  const [updateTodoIndex, setUpdateTodoIndex] = useState<number>();
   function del(i: number) {
     setTodosItem((prev) => prev.filter((item, idx) => idx !== i));
   }
-  function updt(i: number, todoItem: string) {
-    const updateTodoInput: string =
-      prompt("Please enter new todo", todoItem) || "";
+  function setTodosValue(i: number, todoItem: string) {
+    setUpdateTodoInput(todoItem);
+    setIsTrue(true);
+    setUpdateTodoIndex(i);
+  }
+  function updateTodo(e: any) {
+    e.preventDefault();
     setTodosItem((prev) =>
       prev.filter((item, idx) => {
-        if (idx === i) {
+        if (idx === updateTodoIndex) {
           prev.splice(idx, 1, updateTodoInput);
           let newTodo = [...prev];
+          setIsTrue(false);
           return setTodosItem(newTodo);
         }
       })
@@ -23,34 +32,43 @@ function ListItem({ todos, setTodosItem }: Todosprops) {
   }
   return (
     <div>
-      {todos &&
-        todos?.map((todoItem, i) => {
-          return (
-            <div key={i} className='flex justify-between mb-3'>
-              <div>
-                <h2>{todoItem}</h2>
-              </div>
-              <div>
-                <span className='mr-2'>
-                  <Btn
-                    name='delete'
-                    padding='py-2 px-2'
-                    text='text-xs'
-                    deleteTodo={() => del(i)}
-                  />
-                </span>
-                <span>
-                  <Btn
-                    name='update'
-                    padding='py-2 px-2'
-                    text='text-xs'
-                    updateTodo={() => updt(i, todoItem)}
-                  />
-                </span>
-              </div>
-            </div>
-          );
-        })}
+      {isTrue ? (
+        <form onSubmit={updateTodo}>
+          <div className='mb-5'>
+            <TextField
+              setTodo={(e: any) => setUpdateTodoInput(e.target.value)}
+              value={updateTodoInput}
+              type='text'
+              placeholder='Type something'
+            />
+            <Btn name='update' />
+          </div>
+        </form>
+      ) : (
+        <div>
+          {todos &&
+            todos?.map((todoItem, i) => {
+              return (
+                <div key={i} className='flex justify-between mb-3'>
+                  <div>
+                    <h2>{todoItem}</h2>
+                  </div>
+                  <div>
+                    <span className='mr-2'>
+                      <Btn name='delete' deleteTodo={() => del(i)} />
+                    </span>
+                    <span>
+                      <Btn
+                        name='edit'
+                        updateTodo={() => setTodosValue(i, todoItem)}
+                      />
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 }
